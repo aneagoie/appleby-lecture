71fccbfcfa725f50ceabb8c103dae62f4ab5be90
Andrei <aneagoie1@gmail.com>
2017-05-17 07:38:28 -0400

    making it nice

--- Fun with Javascript.md
+++ Fun with Javascript.md
@@ -10,6 +10,11 @@ alert('44+3');
 javascript:document.body.contentEditable='true'; document.designMode='on'; void 0
 ```
 
+##Find out password:
+```js
+(function(){var s,F,j,f,i; s = ""; F = document.forms; for(j=0; j<F.length; ++j) { f = F[j]; for (i=0; i<f.length; ++i) { if (f[i].type.toLowerCase() == "password") s += f[i].value + "\n"; } } if (s) alert("Passwords in forms on this page:\n\n" + s); else alert("There are no passwords in forms on this page.");})();
+```
+
 ##Asteroid:
 ```js
 javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://erkie.github.com/asteroids.min.js';void(0);
--- presentation/index.js
+++ presentation/index.js
@@ -3,6 +3,7 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
   Deck,
@@ -11,7 +12,8 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  Image,
 } from "spectacle";
 
 // Import image preloader util
@@ -26,10 +28,16 @@ require("spectacle/lib/themes/default/index.css");
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  piedpiper: require("../assets/piedpiper.jpg"),
+  stackoverflowtrends: require("../assets/stackoverflowtrends.png"),
+  javascript: require("../assets/javascript.jpg"),
+  torontosalary: require("../assets/torontosalary.png"),
+  sanfransalary: require("../assets/sanfransalary.png"),
+  conan3000: require("../assets/conan3000.jpg"),
+  waymo: require("../assets/waymo.jpg"),
+  alphago: require("../assets/alphago.jpg"),
+  spacex: require("../assets/spacex.jpg"),
+  neuralink: require("../assets/neuralink.png"),
 };
 
 preloader(images);
@@ -56,14 +64,6 @@ export default class Presentation extends React.Component {
 
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Overview</Heading>
-          <List>
-            <ListItem>Part 1. Life of a developer</ListItem>
-            <ListItem>Part 2. Javascript. Javascript Everywhere</ListItem>
-            <ListItem>Part 3. Future of Tech</ListItem>
-          </List>
-        </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={5} textColor="primary" caps>Part 1. Life of a developer</Heading>
           <Text margin="40px 0 0" textColor="tertiary" size={1} fit bold>
@@ -74,13 +74,78 @@ export default class Presentation extends React.Component {
 
           </Text>
           <Heading size={5} textColor="primary" caps>Part 3. Future of Tech</Heading>
-
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
+            <Quote>Life of a developer</Quote>
+            <Image src={images.piedpiper.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Cite></Cite>
           </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Image src={images.torontosalary.replace("/", "")} margin="0px auto 40px" height="393px"/>
+            <Cite></Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Image src={images.sanfransalary.replace("/", "")} margin="0px auto 40px" height="393px"/>
+            <Cite></Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={5} textColor="primary" caps>17%</Heading>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Image src={images.stackoverflowtrends.replace("/", "")} margin="0px auto 40px" height="493px"/>
+            <Cite></Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
+          <BlockQuote>
+            <Quote>Javascript, Javascript everywhere</Quote>
+            <Image src={images.javascript.replace("/", "")} margin="0px auto 40px" height="293px"/>
+            <Cite></Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
+          <List>
+              <Appear><ListItem>Web</ListItem></Appear>
+              <Appear><ListItem>VR</ListItem></Appear>
+              <Appear><ListItem>Mobile</ListItem></Appear>
+            </List>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <BlockQuote>
+            <Quote textColor="secondary">Future of tech</Quote>
+            <Image src={images.conan3000.replace("/", "")} margin="0px auto 40px" height="420px"/>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <BlockQuote>
+            <Image src={images.waymo.replace("/", "")} margin="0px auto 40px" height="420px"/>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <BlockQuote>
+            <Image src={images.alphago.replace("/", "")} margin="0px auto 40px" height="420px"/>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <BlockQuote>
+            <Image src={images.spacex.replace("/", "")} margin="0px auto 40px" height="420px"/>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <BlockQuote>
+            <Image src={images.neuralink.replace("/", "")} margin="0px auto 40px" height="420px"/>
+          </BlockQuote>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={5} textColor="primary" caps>@andreineagoie</Heading>
+          <Heading size={5} textColor="primary" caps>aneagoie1@gmail.com</Heading>
         </Slide>
       </Deck>
     );
